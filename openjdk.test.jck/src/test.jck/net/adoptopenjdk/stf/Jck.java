cf554feac2cffc96928bff2e5cf42c3dd11545a4
Soumya Biradar <soumyabiradar@Soumyas-MacBook-Pro.local>
2022-09-07 12:28:39 +0530

    reverting back jdwp related changes

--- openjdk.test.jck/src/test.jck/net/adoptopenjdk/stf/Jck.java
+++ openjdk.test.jck/src/test.jck/net/adoptopenjdk/stf/Jck.java
@@ -710,11 +710,6 @@ public class Jck implements StfPluginInterface {
 					fileContent += "set jck.env.runtime.remoteAgent.passiveHost localhost" + ";\n";
 					fileContent += "set jck.env.runtime.remoteAgent.passivePortDefault Yes" + ";\n";
 					
-					if(tests.contains("vm/jdwp"))  {
-					
-					fileContent += "set jck.env.runtime.jdwp.VMSuspended No" + ";\n";
-					fileContent += "set jck.env.runtime.jdwp.jdwpOpts -agentlib\:jdwp\=server\=y,transport\=dt_socket,address\=localhost\:35000,suspend\=n"  + ";\n";
-					}
 				}
 			}
 			// Without the following override the following failures occur:
